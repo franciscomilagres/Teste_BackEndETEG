dcc27cfa9c156c82dfbdd67e7871b2203cddf875
franciscomilagres <francisco.lima93@yahoo.com.br>
2019-01-31 14:34:16 -0200

    Iniciando projeto

---
+++ butterflymovies/src/main/java/com/butterflymovies/butterflymovies/ButterflymoviesApplication.java
@@ -0,0 +1,14 @@
+package com.butterflymovies.butterflymovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButterflymoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButterflymoviesApplication.class, args);
+	}
+
+}
+
---
+++ butterflymovies/src/test/java/com/butterflymovies/butterflymovies/ButterflymoviesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.butterflymovies.butterflymovies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ButterflymoviesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
